11f3538f7103f50d284a9c28b23e85d10ffa4258
Manfred <manfredcb@gmail.com>
2023-12-13 17:38:27 -0300

    fix add allowed header cors

--- src/main/java/ar/com/codoacodo/filters/CorsFilter.java
+++ src/main/java/ar/com/codoacodo/filters/CorsFilter.java
@@ -14,11 +14,13 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         List<String> allowedOrigins = List.of("http://127.0.0.1:5500", "http://localhost:5500");
-
+        String allowHeaders = "Content-Type";
         String origin = ((HttpServletRequest) request).getHeader("origin");
 
+        HttpServletResponse res = ((HttpServletResponse) response);
         if(origin != null && allowedOrigins.contains(origin)){
-            ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", origin);
+            res.addHeader("Access-Control-Allow-Origin", origin);
+            res.setHeader("Access-Control-Allow-Headers", allowHeaders);
         }
 
         filterChain.doFilter(request, response);
